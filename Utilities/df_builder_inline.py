ddde39aa4fc132c058a125a08219f803fd6872dd
TheAlchemist <62193858+Data-Alchemy@users.noreply.github.com>
2021-12-21 20:06:26 -0800

    Update df_builder_inline.py

--- Utilities/df_builder_inline.py
+++ Utilities/df_builder_inline.py
@@ -1,69 +1,53 @@
-
-from pyspark.sql import DataFrame as SparkDataFrame
 def df_builder_inline(type: str,file_format:str , zone:str , subject_area: str , directory: str, file_name:str = None, opts:str = None , gen_schema:bool =False) ->SparkDataFrame:
   ### csv files are the worst so we need to set header option to read the file correctly ###
   try:
       ######################################################################################
-      ##################    finds latest file in adls to read  from    #####################
-      if type== "latest":
-        file_list = recursive_ls(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}")
-        latest_file = f"/dbfs{file_list[-1][5:]}"
-
+      ##################       finds  file in adls to read  from       #####################
+      if file_name != None: 
+        option = None
+        file_name =f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}"
+        schema_file = f"/dbfsmnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}"
       
+      if type== "latest":
+        option = None
+        file_list   = recursive_ls(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}")
+        latest_file = f"/dbfs{file_list[-1][5:]}"
+        file_name   =  f"dbfs:{file_list[-1][5:]}"
+        schema_file = f"/dbfs{file_list[-1][5:]}"
+        #f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}"
+        
+        
+      if type == "all":
+        option = '"recursiveFileLookup", "true"'
+        file_name = f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/"
+        
+        file_list   = recursive_ls(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}")
+        schema_file = f"/dbfs{file_list[-1][5:]}"
       ######################################################################################
       ##############   generates parquet schema without reading data      ##################
       
       if type != 'all' and file_format == "parquet": 
-        myschema = read_parquet_schema_df(path= f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
+        myschema = read_parquet_schema_df(path= schema_file)
         parquet_schema= PySparkParquetSchema(Schema = myschema)
-      elif type == 'all' and file_format == file_format == "parquet":   
-        schema_files = recursive_ls(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}")
-        latest_schema_file = f"/dbfs{schema_files[-1][5:]}"
-        myschema = read_parquet_schema_df(path= latest_schema_file)
+      elif type == 'all' and file_format  == "parquet":   
+        myschema = read_parquet_schema_df(path= schema_file)
         parquet_schema= PySparkParquetSchema(Schema = myschema)
                                                           #f"/mnt/adls_sofdl{zone}_{subject_area}/{directory}/*."+file_format)
       ######################################################################################  
       
       if file_format == "csv":
-        if file_name != None:  
-          return spark.read.format(f"{file_format}").option("header","true").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-        elif type == 'daily':
-          return  spark.read.format(f"{file_format}").option("header","true").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-        elif type == 'all':
-            return spark.read.format(f"{file_format}").option("recursiveFileLookup", "true").option("header","true").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}")
-
+          return spark.read.format(f"{file_format}").option("header","true").load(file_name)
+        
       elif file_format == "parquet" and gen_schema ==True:
-        if file_name != None:  
-            return spark.read.option("inferSchema" , "false").format(f"{file_format}").schema(parquet_schema).load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-          #spark.read.format(f"{file_format}").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-        elif type == 'daily':
-            return  spark.read.option("inferSchema" , "false").format(f"{file_format}").schema(parquet_schema).load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-        elif type == 'all':
-            return spark.read.option("inferSchema" , "false").format(f"{file_format}").schema(parquet_schema).option("recursiveFileLookup", "true").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/")
+            return spark.read.option("inferSchema" , "false").format(f"{file_format}").schema(parquet_schema).option(option).load(file_name)
 
       elif file_format == "parquet" and gen_schema ==False:
-        if file_name != None:  
-            return spark.read.option("inferSchema" , "True").format(f"{file_format}").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-          #spark.read.format(f"{file_format}").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-        elif type == 'daily':
-            return  spark.read.option("inferSchema" , "True").format(f"{file_format}").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-        elif type == 'all':
-            return spark.read.format(f"{file_format}").option("recursiveFileLookup", "true").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}")
-          
-      elif file_format == "delta" and gen_schema ==False:
-        if file_name != None:  
-            return spark.read.option("inferSchema" , "true").format(f"{file_format}").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-          #spark.read.format(f"{file_format}").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-        elif type == 'daily':
-            return  spark.read.option("inferSchema" , "true").format(f"{file_format}").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/{file_name}")
-        elif type == 'all':
-            return spark.read.option("inferSchema" , "true").format(f"{file_format}").option("recursiveFileLookup", "true").load(f"dbfs:/mnt/adls_sofdl{zone}_{subject_area}/{directory}/")
-
+            return spark.read.option("inferSchema" , "True").format(f"{file_format}").load(file_name)
+    
+      elif file_format == "delta" and gen_schema ==False: 
+            return spark.read.option("inferSchema" , "true").format(f"{file_format}").option(option).load(file_name)
       else:
         print("file ext not recognized for df transform")
   except Exception as e: # work on python 3.x
-       print(f'File ext not recognized for df transform for {directory}{file_name}: ', str(e))
+       print(f'File ext not recognized for df transform for directory: {directory} \n file :{file_name} \n exception message: : ', str(e))
       
-    
-'''sqlContext.setConf("spark.sql.parquet.compression.codec", "snappy")'''
-
