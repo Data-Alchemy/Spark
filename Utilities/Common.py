b1ba6211dcf57c44077a68f2cbf625128cbb68f8
TheAlchemist <62193858+Data-Alchemy@users.noreply.github.com>
2021-12-21 20:06:11 -0800

    Update Common.py

--- Utilities/Common.py
+++ Utilities/Common.py
@@ -19,6 +19,10 @@ def build_struct(col,dtype):
     data_type = StringType()
   return StructField(col,data_type)
 
+def spark_shape(self):
+    return {'rows':'{:,}'.format(self.count()), 'columns':len(self.columns)}
+pyspark.sql.dataframe.DataFrame.shape = spark_shape
+
 def read_parquet_schema_df(path: str, column_name_length: int = 50 ) -> dict:#pd.DataFrame:
     illegal_dtypes = ["list<item: null>","null","list<item: string>"] # list of data types not supported in spark df
     schema = pq.read_schema(path, memory_map=True)
@@ -47,9 +51,29 @@ def PySparkParquetSchema(Schema):
   parquet_schema = StructType(struct)
   return parquet_schema
 
-def recursive_ls(ls_path):
-  dir_paths = dbutils.fs.ls(ls_path)
-  subdir_paths = [get_dir_content(p.path) for p in dir_paths if p.isDir() and p.path != ls_path]
-  flat_subdir_paths = [p for subdir in subdir_paths for p in subdir]
+def recursive_ls(ls_path): #version 2 #
+  dir_paths= dbutils.fs.ls(ls_path)
+  subdir_paths =[p.path for p in dir_paths if p.isDir() and 'UNPROCESSED' not in p.path and int(p.size) !=0]
+  if len(subdir_paths) <1 :
+    flat_subdir_paths = [p.path for p in dir_paths if p.isDir()==False and int(p.size) !=0]
+  else: 
+    flat_subdir_paths = [p for subdir in subdir_paths for p in subdir if int(p.size) !=0]
   return list(map(lambda p: p.path, dir_paths)) + flat_subdir_paths
+
+date = spark.sql(""" 
+Select 
+date_format(now()+ INTERVAL -7 hours,'yyyy') as Year,
+date_format(now()+ INTERVAL -7 hours,'MM') as Month,
+date_format(now()+ INTERVAL -7 hours,'yyyy-MM-dd HH:mm:ss') as Date,
+date_format(now()+ INTERVAL -7 hours,'yyyy/MM') as PathDate,
+date_format(date_add(now()+ INTERVAL -7 hours,-2),'yyyyMMdd')||'_'||date_format(date_add(now()+ INTERVAL -7 hours,-1),'yyyyMMdd') as date_range,
+date_format(now()+ INTERVAL -7 hours,'HH:mm') as time
+""").collect()[0]
+
+processing_date = date["Date"]
+processing_time = date["time"]
+daily_file_path= date["PathDate"]
+daily_range = date["date_range"]
+daily_year = date["Year"]
+daily_month =date["Month"]
 
